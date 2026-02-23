1844e354b32fd6cbd1fabcaf8c231a003cd17af2
Camilo117A <camilocaviedes4@gmail.com>
2026-02-23 14:52:15 -0500

    Update Calculadora_de_una_tienda.java

--- Ejercicios_Conceptos_Básicos/Calculadora_de_una_tienda.java
+++ Ejercicios_Conceptos_Básicos/Calculadora_de_una_tienda.java
@@ -1,19 +1,19 @@
-public class CalculadoraTienda {
+public class Calculadora_de_una_tienda {
     public static void main(String[] args) {
         // Datos de entrada
-        double precioUnitario = 1500.0;
-        int cantidadComprada = 5;
-        double descuentoPorcentaje = 10.0;
+        double precio_U = 1500.0;
+        int cantidad_C = 5;
+        double descuento_P = 10.0;
 
         // Procesamiento de datos
-        double subtotal = precioUnitario * cantidadComprada;
-        double descuentoCalculado = subtotal * (descuentoPorcentaje / 100);
+        double subtotal = precio_U * cantidad_C;
+        double descuentoCalculado = subtotal * (descuento_P / 100);
         double totalFinal = subtotal - descuentoCalculado;
 
         // Salida de resultados
         System.out.println("=== Detalle de Compra ===");
         System.out.println("Subtotal: $" + subtotal);
-        System.out.println("Descuento (" + descuentoPorcentaje + "%): $" + descuentoCalculado);
+        System.out.println("Descuento (" + descuento_P + "%): $" + descuentoCalculado);
         System.out.println("Total a pagar: $" + totalFinal);
     }
 }
