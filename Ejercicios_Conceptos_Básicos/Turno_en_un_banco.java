e39ba02cbacac5c0fbe028fb3d6eec02d148a369
Camilo117A <camilocaviedes4@gmail.com>
2026-02-23 21:46:03 -0500

    Update Turno_en_un_banco.java

--- Ejercicios_Conceptos_Básicos/Turno_en_un_banco.java
+++ Ejercicios_Conceptos_Básicos/Turno_en_un_banco.java
@@ -1,1 +1,15 @@
+public class Turno_en_un_banco {
+    public static void main(String[] args) {
 
+        int turno_Actual = 15;
+        int turno_En_Pantalla = turno_Actual;
+
+        System.out.println("Turno actual: " + turno_Actual);
+        System.out.println("Turno en pantalla: " + turno_En_Pantalla);
+
+        turno_En_Pantalla = 99;
+
+        System.out.println("\nTurno actual: " + turno_Actual);
+        System.out.println("Turno en pantalla: " + turno_En_Pantalla);
+    }
+}
