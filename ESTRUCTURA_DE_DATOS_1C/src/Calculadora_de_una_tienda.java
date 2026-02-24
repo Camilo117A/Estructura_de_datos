76b74c247e6c759aaab53159a2e3a301c268a020
Camilo117A <camilocaviedes4@gmail.com>
2026-02-24 06:40:20 -0500

    arreglo

--- ESTRUCTURA_DE_DATOS_1C/src/Calculadora_de_una_tienda.java
+++ ESTRUCTURA_DE_DATOS_1C/src/Calculadora_de_una_tienda.java
@@ -1,3 +1,12 @@
+//Ejercicio 2 — Calculadora de una tienda
+//Una tienda de barrio vende productos al por menor. El cajero necesita calcular:
+
+//Precio unitario de un producto (double)
+//Cantidad comprada (int)
+//Descuento aplicado en porcentaje (double)
+//Total a pagar después del descuento
+//Declara las variables, realiza el cálculo y muestra el resultado por consola.
+
 public class Calculadora_de_una_tienda {
     public static void main(String[] args) {
         // Datos de entrada
--- ESTRUCTURA_DE_DATOS_1C/src/Historia_de_mensajes.java
+++ ESTRUCTURA_DE_DATOS_1C/src/Historia_de_mensajes.java
@@ -1,3 +1,13 @@
+//Ejercicio 8 — Historial de mensajes
+//Un chat guarda el último mensaje enviado. Se usan dos variables String:
+
+//ultimoMensaje = "Hola"
+//copiaTexto = ultimoMensaje
+//Luego copiaTexto se cambia a "¿Cómo estás?"
+//Muestra ambas variables y explica en un comentario por qué ultimoMensaje no cambió (inmutabilidad de String).
+
+
+
 public class Historia_de_mensajes {
     public static void main(String[] args) {
 
