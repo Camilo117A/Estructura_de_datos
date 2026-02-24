6dd9439275c1bbcc347f78072c38c1b5dc4a3a46
Camilo117A <camilocaviedes4@gmail.com>
2026-02-23 21:50:15 -0500

    Update Inventari_compartido.java

--- Ejercicios_Conceptos_Básicos/Inventari_compartido.java
+++ Ejercicios_Conceptos_Básicos/Inventari_compartido.java
@@ -1,1 +1,20 @@
+public class Inventari_compartido {
+    public static void main(String[] args) {
 
+        int[] stock_Almacen = {100, 50, 75, 30, 60};
+        int[] stock_Reportes = stock_Almacen;
+
+        System.out.println("Stock almacen antes de la venta: ");
+        for (int stock : stock_Almacen) {
+            System.out.print(stock + " ");
+        }
+
+        // Se simula una venta, se reduce el stock del producto 1
+        stock_Reportes[0] = 80;
+
+        System.out.println("\n\nStock almacen despues de la venta: ");
+        for (int stock : stock_Almacen) {
+            System.out.print(stock + " ");
+        }
+    }
+}
